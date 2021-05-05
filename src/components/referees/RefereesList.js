5942ac970e35bccdad338b8712b671d87291da3c
Adolfo <allorenter@gmail.com>
2021-05-05 11:10:01 +0200

    Cambio para visualizar error cuando la competición no tiene datos de arbitros

--- src/components/referees/RefereesList.js
+++ src/components/referees/RefereesList.js
@@ -4,35 +4,46 @@ import { StyledTable } from "../../styles/styles";
 import RefereeDataTableRow from "./RefereeDataTableRow";
 import Loading from "../Loading";
 import { darkenOpacity, lightenOpacity } from "../../styles/styles";
+import Alert from '@material-ui/lab/Alert';
+import { styled } from "@material-ui/core/styles";
 
 const RefereesList = (props) => {
+  const StyledAlert = styled(Alert)({
+    margin: "10vh auto",
+  });
+  
   const classes = props.loading ? darkenOpacity() : lightenOpacity(); 
-  console.log("REFEREESDATA", props.refereesData);
+  
   if((!props.refereesData || props.refereesData.length === 0) && props.loading === false){
-      return (<div>NO HAY DATOS SOBRE ARBITROS DISPONIBLES PARA ESA COMPETICIÓN</div>);
+    return (
+      <StyledAlert severity="warning">NO HAY DATOS SOBRE ARBITROS DISPONIBLES EN ESTA COMPETICIÓN</StyledAlert>
+    );  
   }
+  
   return (
     <TableContainer>
-      {props.loading && <Loading boxHeight="45vh" boxWidth="81%" />}
-      <StyledTable size="small" aria-label="Tabla" className={classes.root}>
-          <TableHead>
-            <TableRow>
-              <TableCell className="c1"></TableCell>
-              <TableCell colSpan={3} className="c2">
-                Amarillas
-              </TableCell>
-              <TableCell colSpan={3} className="c1">
-                Rojas
-              </TableCell>
-            </TableRow>
-            <RefereeDataTableRow subHeader={true} />
-          </TableHead>
-          <TableBody>
-            {props.refereesData?.map((row) => (
-              <RefereeDataTableRow data={row} />
-            ))}
-          </TableBody>
-        </StyledTable>
+      {props.loading 
+        ? <Loading boxHeight="45vh" boxWidth="81%" />
+        : <StyledTable size="small" aria-label="Tabla" className={classes.root}>
+            <TableHead>
+              <TableRow>
+                <TableCell className="c1"></TableCell>
+                <TableCell colSpan={3} className="c2">
+                  Amarillas
+                </TableCell>
+                <TableCell colSpan={3} className="c1">
+                  Rojas
+                </TableCell>
+              </TableRow>
+              <RefereeDataTableRow subHeader={true} />
+            </TableHead>
+            <TableBody>
+              {props.refereesData?.map((row) => (
+                <RefereeDataTableRow data={row} />
+              ))}
+            </TableBody>
+          </StyledTable>
+      }
     </TableContainer>
   );
 };
